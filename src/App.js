e86d3c41c8e4e5ebf55b7b06210d2d8c6c653b2f
sreecharanbezawada <sreecharanipsofficer@gmail.com>
2023-09-19 05:43:19 +0000

    Tech-era-Interview - Mock - Exam - 1A

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/extensions */
+import {Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+import Header from './components/Header'
+import CourseItemDetails from './components/CourseItemDetails'
+// eslint-disable-next-line import/no-unresolved
+import NotFound from './components/NotFound'
+import './App.css'
+
+const App = () => (
+  <>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/courses/:id" component={CourseItemDetails} />
+      <Route component={NotFound} />
+    </Switch>
+  </>
+)
+
+export default App
